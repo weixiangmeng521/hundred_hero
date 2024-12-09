0c9c6d2ce05a84653b1b084fba2a879786fe335d
优孟 <397317382@qq.com>
2024-12-10 08:58:35 +0900

    fix: 修复

--- main.py
+++ main.py
@@ -147,9 +147,9 @@ def main():
 
 # TODO: 获取当前钱的数量,并放入缓存里
 if __name__ == "__main__":
-    # main()
+    main()
 
-    webServer.run(queue)
+    # webServer.run(queue)
 
     # vc.test_for_find_object_in_image()
     # print(pyautogui.position())
